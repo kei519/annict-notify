01807ab94a29310ab3ea68473c49f4293a8d9cfb
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-10-16 21:06:57 +0900

    feat: tracing によるログ機能の追加

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::env;
 
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
 #[tokio::main]
 async fn main() {
+    // ロガーの設定
+    tracing_subscriber::registry()
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| env!("CARGO_CRATE_NAME").into()))
+        .with(fmt::layer())
+        .init();
+
     // .env の読み込み
     dotenv::dotenv().ok();
 
