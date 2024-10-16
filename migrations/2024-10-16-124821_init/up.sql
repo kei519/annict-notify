f176f3d49227a49610cc02b9b40a4e8a27f419fc
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-10-16 21:55:02 +0900

    feat: 必要最低限の情報の migrations

---
+++ migrations/2024-10-16-124821_init/up.sql
@@ -0,0 +1,16 @@
+-- Your SQL goes here
+CREATE TABLE channels (
+    guild_id BIGINT PRIMARY KEY,
+    channel_id BIGINT NOT NULL
+);
+
+CREATE TABLE subscribers (
+    id SERIAL PRIMARY KEY,
+    -- discord
+    user_id BIGINT NOT NULL,
+    guild_id BIGINT NOT NULL,
+    -- annict
+    annict_name TEXT NOT NULL,
+    end_cursor TEXT,
+    last_activity_date TIMESTAMP (0) WITH TIME ZONE
+);
