cee2c1e534415f0fbfd0a553153c7593cca18c4c
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-10-16 23:23:08 +0900

    feat: 実行時に反映されていないマイグレーションを実行

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-use std::{env, error::Error};
+use std::{
+    env::{self, VarError},
+    error::Error,
+};
+
+use diesel::{Connection, PgConnection};
+use diesel_migrations::{EmbeddedMigrations, MigrationHarness};
 
 mod schema;
 
 pub type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
+const MIGRATIONS: EmbeddedMigrations = diesel_migrations::embed_migrations!("./migrations");
+
 pub async fn main() -> Result<()> {
-    let Ok(envvar) = env::var("ENV_VAR") else {
-        return Err("環境変数 `ENV_VAR` を設定してください".into());
-    };
-    println!("{}", envvar);
+    // 反映されていないマイグレーションの実行
+    let mut conn = PgConnection::establish(&get_env("DATABASE_URL")?)?;
+    conn.run_pending_migrations(MIGRATIONS)?;
 
     Ok(())
+}
+
+/// 環境変数 `key` を取り出す。
+/// ただし、存在しなかった場合は分かりやすいエラーメッセージを表示するエラーを返す。
+pub fn get_env(key: impl AsRef<str>) -> Result<String> {
+    match env::var(key.as_ref()) {
+        Err(VarError::NotPresent) => {
+            Err(format!("環境変数 `{}` を設定してください", key.as_ref()).into())
+        }
+        other => Ok(other?),
+    }
 }
