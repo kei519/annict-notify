2464080c38cba25f6fb575643557cf3d12bfd172
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-10-17 12:00:56 +0900

    feat: 通知間隔を設定できるように

--- src/discord.rs
+++ src/discord.rs
@@ -1,9 +1,11 @@
-use std::{future::Future, sync::Arc};
+use std::{future::Future, sync::Arc, time::Duration};
 
+use regex::Regex;
 use serenity::{
     all::{EventHandler, GatewayIntents, Http},
     Client,
 };
+use tokio::time;
 
 use crate::{get_env, Result};
 
@@ -21,10 +23,37 @@ pub async fn start() -> Result<(impl Future<Output = Result<()>>, Arc<Http>)> {
 
 /// 通知に用いる [Http] クライアントを受け取り、通知タスクを開始する。
 pub async fn notify(_http: Arc<Http>) -> Result<()> {
-    todo!()
+    let interval = get_interval()?;
+    tracing::info!("更新間隔: {} 秒", interval.as_secs());
+    loop {
+        tracing::trace!("loop!");
+        time::sleep(interval).await;
+    }
 }
 
 pub struct Handler;
 
 #[serenity::async_trait]
 impl EventHandler for Handler {}
+
+fn get_interval() -> Result<Duration> {
+    let duration = get_env("NOTIFICATION_INTERVAL")?;
+    let regex = Regex::new(r"^\s*(\d+)\s*((?i)s|sec|m|min|h|hour)\s*$")?;
+    let (_, [num, unit]) = regex
+        .captures(&duration)
+        .ok_or_else(|| {
+            format!(
+                "環境変数 `NOTIFICATION_INTERVAL` (\"{}\") の形式が不正です",
+                duration
+            )
+        })?
+        .extract();
+
+    let num: u64 = num.parse()?;
+    Ok(match unit.to_ascii_lowercase().as_str() {
+        "s" | "sec" => Duration::from_secs(num),
+        "m" | "min" => Duration::from_secs(num * 60),
+        "h" | "hour" => Duration::from_secs(num * 60 * 60),
+        _ => unreachable!("正規表現の不正"),
+    })
+}
