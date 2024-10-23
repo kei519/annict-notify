007c1d4d60d6723c443237b65c50f922b0b3bdd9
kei519 <masaki.keigo.q00@kyoto-u.jp>
2024-10-23 20:59:05 +0900

    feat: db mod に subscribers のための要素を追加

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,15 @@
-use diesel::{Connection, ExpressionMethods, PgConnection, QueryResult, RunQueryDsl};
+use chrono::{DateTime, TimeZone};
+use diesel::{
+    query_dsl::methods::FilterDsl, Connection, ExpressionMethods, PgConnection, QueryResult,
+    RunQueryDsl,
+};
 
-use crate::{get_env, models::Channel, schema::*, Result};
+use crate::{
+    get_env,
+    models::{Channel, NewSubscriber, Subscriber},
+    schema::*,
+    Result,
+};
 
 pub fn connect() -> Result<PgConnection> {
     Ok(PgConnection::establish(&get_env("DATABASE_URL")?)?)
@@ -21,4 +30,59 @@ pub fn insert_or_update_channel(
         .do_update()
         .set(channels::channel_id.eq(new_chan.channel_id))
         .get_result(conn)
+}
+
+pub fn get_channels(conn: &mut PgConnection) -> QueryResult<Vec<Channel>> {
+    channels::table.load(conn)
+}
+
+pub fn insert_or_update_subscriber(
+    conn: &mut PgConnection,
+    user_id: u64,
+    guild_id: u64,
+    annict_name: impl AsRef<str>,
+    end_cursor: Option<&str>,
+    last_activity_date: Option<DateTime<impl TimeZone>>,
+) -> QueryResult<Subscriber> {
+    let new_subscriber = NewSubscriber {
+        user_id: user_id as _,
+        guild_id: guild_id as _,
+        annict_name: annict_name.as_ref(),
+        end_cursor,
+        last_activity_date: last_activity_date.map(|dt| dt.to_utc()),
+    };
+
+    diesel::insert_into(subscribers::table)
+        .values(&new_subscriber)
+        .on_conflict((subscribers::guild_id, subscribers::user_id))
+        .do_update()
+        .set((
+            subscribers::annict_name.eq(new_subscriber.annict_name),
+            subscribers::end_cursor.eq(new_subscriber.end_cursor),
+            subscribers::last_activity_date.eq(new_subscriber.last_activity_date),
+        ))
+        .get_result(conn)
+}
+
+pub fn update_subscriber_info(
+    conn: &mut PgConnection,
+    id: i32,
+    end_cursor: Option<&str>,
+    last_activity_date: Option<DateTime<impl TimeZone>>,
+) -> QueryResult<Subscriber> {
+    diesel::update(subscribers::table.filter(subscribers::id.eq(id)))
+        .set((
+            subscribers::end_cursor.eq(end_cursor),
+            subscribers::last_activity_date.eq(last_activity_date),
+        ))
+        .get_result(conn)
+}
+
+pub fn get_subscribers_by_guild(
+    conn: &mut PgConnection,
+    guild_id: u64,
+) -> QueryResult<Vec<Subscriber>> {
+    subscribers::table
+        .filter(subscribers::guild_id.eq(guild_id as i64))
+        .load(conn)
 }
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use diesel::prelude::{Insertable, Queryable};
 
 use super::schema::*;
@@ -7,4 +8,24 @@ use super::schema::*;
 pub struct Channel {
     pub guild_id: i64,
     pub channel_id: i64,
+}
+
+#[derive(Debug, Queryable, PartialEq, Eq)]
+pub struct Subscriber {
+    pub id: i32,
+    pub user_id: i64,
+    pub guild_id: i64,
+    pub annict_name: String,
+    pub end_cursor: Option<String>,
+    pub last_activity_date: Option<DateTime<Utc>>,
+}
+
+#[derive(Debug, Insertable)]
+#[diesel(table_name = subscribers)]
+pub struct NewSubscriber<'a, 'b> {
+    pub user_id: i64,
+    pub guild_id: i64,
+    pub annict_name: &'a str,
+    pub end_cursor: Option<&'b str>,
+    pub last_activity_date: Option<DateTime<Utc>>,
 }
